b998768744dccea469d9996316284fa53886b558
Favorskij <55238658+Favorskij@users.noreply.github.com>
2022-05-16 17:55:55 +1100

    Update my.sql

--- my.sql
+++ my.sql
@@ -1,1 +1,5 @@
+Выборка среднего возраста округлённую в меньшию сторону.
 
+SELECT FLOOR(AVG(TIMESTAMPDIFF(YEAR, birthday, curdate()))) AS age FROM FamilyMembers
+
+curdate() тоже самое что и CURRENT_DATE
